965fe74570a67b36a0ce1926f52a5d1377180366
Aladdin-2 <aladdin19.11.21@gmail.com>
2025-05-19 15:57:11 +0400

    Update User.java

--- src/main/java/com/aladdin/lesson5vcs/User.java
+++ src/main/java/com/aladdin/lesson5vcs/User.java
@@ -5,8 +5,5 @@ import lombok.Data;
 @Data
 public class User {
     private String userName;
-    private String text;
-    private Long phoneNumber;
-    private String address;
     private String password;
 }
